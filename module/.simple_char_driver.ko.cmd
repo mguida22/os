e6c959d9c320adc322fff3e8f7660d277b813b61
mguida22 <michael.guida@colorado.edu>
2016-03-13 21:39:50 -0600

    add given files

---
+++ module/.simple_char_driver.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/user/Documents/os/module/simple_char_driver.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/user/Documents/os/module/simple_char_driver.ko /home/user/Documents/os/module/simple_char_driver.o /home/user/Documents/os/module/simple_char_driver.mod.o
---
+++ module/.tmp_versions/simple_char_driver.mod
@@ -0,0 +1,2 @@
+/home/user/Documents/os/module/simple_char_driver.ko
+/home/user/Documents/os/module/simple_char_driver.o
