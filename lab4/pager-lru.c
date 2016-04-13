ae226e43356eec7f9fd79f2d055e7bdee1739f96
mguida22 <michael.guida@colorado.edu>
2016-04-13 00:20:25 -0600

    lru working

--- lab4/pager-lru.c
+++ lab4/pager-lru.c
@@ -1,33 +1,14 @@
-/*
- * File: pager-lru.c
- * Author:       Andy Sayler
- *               http://www.andysayler.com
- * Adopted From: Dr. Alva Couch
- *               http://www.cs.tufts.edu/~couch/
- *
- * Project: CSCI 3753 Programming Assignment 4
- * Create Date: Unknown
- * Modify Date: 2012/04/03
- * Description:
- * 	This file contains an lru pageit
- *      implmentation.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "simulator.h"
 
+/* Static vars */
+static int initialized = 0;
+static int tick = 1; // artificial time
+static int timestamps[MAXPROCESSES][MAXPROCPAGES];
+
 void pageit(Pentry q[MAXPROCESSES]) {
-
-  /* This file contains the stub for an LRU pager */
-  /* You may need to add/remove/modify any part of this file */
-
-  /* Static vars */
-  static int initialized = 0;
-  static int tick = 1; // artificial time
-  static int timestamps[MAXPROCESSES][MAXPROCPAGES];
-
   /* Local vars */
   int proctmp;
   int pagetmp;
@@ -42,9 +23,47 @@ void pageit(Pentry q[MAXPROCESSES]) {
     initialized = 1;
   }
 
-  /* TODO: Implement LRU Paging */
-  fprintf(stderr, "pager-lru not yet implemented. Exiting...\n");
-  exit(EXIT_FAILURE);
+  int proc;
+  int pc;
+  int page;
+  int old_page;
+  int old_page_time;
+  int curr_page;
+
+  for (proc = 0; proc < MAXPROCESSES; proc++) {
+    // if process is not active, move along
+    if (!q[proc].active) {
+      continue;
+    }
+
+    pc = q[proc].pc;
+    page = pc / PAGESIZE;
+
+    // set the timestamp of when we used this
+    timestamps[proc][page] = tick;
+
+    // if page is swapped out
+    if(!q[proc].pages[page]) {
+      // try to swap it in
+      if(!pagein(proc,page)) {
+        // if we can't swap in, swap out oldest page
+        // tick + 1 is ALWAYS greater than any timestamp
+        old_page_time = tick + 1;
+        for (curr_page = 0; curr_page < q[proc].npages; curr_page++) {
+          // if this is the oldest, see if we can swap out
+          if (timestamps[proc][curr_page] < old_page_time) {
+            // if this page isn't in memory, no reason to swap out
+            if (q[proc].pages[curr_page]) {
+              old_page_time = timestamps[proc][curr_page];
+              old_page = curr_page;
+            }
+          }
+        }
+
+        pageout(proc, old_page);
+      }
+    }
+  }
 
   /* advance time for next pageit iteration */
   tick++;
