a35152c702a203acf20cdeb0fc66dd10b567b638
mguida22 <michael.guida@colorado.edu>
2016-04-13 10:36:23 -0600

    fancy stuff that dont work

--- lab4/pager-predict.c
+++ lab4/pager-predict.c
@@ -3,36 +3,53 @@
 
 #include "simulator.h"
 
-struct node {
-  int process;
-  int page;
-  int timestamp;
-};
-
 /* Static vars */
 static int initialized = 0;
 static int tick = 1; // artificial time
-static struct node used[PHYSICALPAGES];
-static int used_count = 0;
+static int timestamps[MAXPROCESSES][MAXPROCPAGES];
+
+void swapLRU(int proc, int page, int tick, Pentry q[MAXPROCESSES],
+             int timestamps[MAXPROCESSES][MAXPROCPAGES]) {
+
+  int curr_page;
+  int old_page;
+  // if we can't swap in, swap out oldest page
+  // tick + 1 is ALWAYS greater than any timestamp
+  int old_page_time = tick + 1;
+  for (curr_page = 0; curr_page < q[proc].npages; curr_page++) {
+    // if this is the oldest, see if we can swap out
+    if (timestamps[proc][curr_page] < old_page_time) {
+      // if this page isn't in memory, no reason to swap out
+      if (q[proc].pages[curr_page]) {
+        old_page_time = timestamps[proc][curr_page];
+        old_page = curr_page;
+      }
+    }
+  }
+
+  pageout(proc, old_page);
+  pagein(proc, page);
+}
 
 void pageit(Pentry q[MAXPROCESSES]) {
+  /* Local vars */
+  int proctmp;
+  int pagetmp;
+
+  /* initialize static vars on first run */
+  if(!initialized){
+    for(proctmp=0; proctmp < MAXPROCESSES; proctmp++){
+      for(pagetmp=0; pagetmp < MAXPROCPAGES; pagetmp++){
+        timestamps[proctmp][pagetmp] = 0;
+      }
+    }
+    initialized = 1;
+  }
+
   int proc;
   int pc;
   int page;
-  int old;
-  int old_page_time;
   int i;
-
-  /* initialize static vars on first run */
-  if (!initialized) {
-    for (i = 0; i < PHYSICALPAGES; i++) {
-      used[i].process = -1;
-      used[i].page = -1;
-      used[i].timestamp = 2147483647;
-    }
-
-    initialized = 1;
-  }
 
   for (proc = 0; proc < MAXPROCESSES; proc++) {
     // if process is not active, move along
@@ -40,42 +57,18 @@ void pageit(Pentry q[MAXPROCESSES]) {
       continue;
     }
 
-    pc = q[proc].pc;
-    page = pc / PAGESIZE;
+    for (i = 0; i < 5; i++) {
+      pc = q[proc].pc + i;
+      page = pc / PAGESIZE;
 
-    // if page is swapped out
-    if (!q[proc].pages[page]) {
-      // try to swap it in
-      if (pagein(proc, page)) {
-        used[used_count].process = proc;
-        used[used_count].page = page;
-        used[used_count].timestamp = tick;
+      // set the timestamp of when we used this
+      timestamps[proc][page] = tick;
 
-        used_count++;
-      } else {
-        // if we can't swap in, swap out oldest page
-        // tick + 1 is ALWAYS greater than any timestamp
-        old_page_time = tick + 1;
-        for (i = 0; i < PHYSICALPAGES; i++) {
-          // if this is the oldest, see if we can swap out
-          if (used[i].timestamp < old_page_time) {
-            old_page_time = used[i].timestamp;
-            old = i;
-          }
-        }
-
-        if (!pageout(used[old].process, used[old].page)) {
-            printf("out\n");
-            exit(1);
-        }
-
-        used[old].process = proc;
-        used[old].page = page;
-        used[old].timestamp = tick;
-
+      // if page is swapped out
+      if (!q[proc].pages[page]) {
+        // try to swap it in
         if (!pagein(proc, page)) {
-            printf("in\n");
-            exit(1);
+          swapLRU(proc, page, tick, q, timestamps);
         }
       }
     }
